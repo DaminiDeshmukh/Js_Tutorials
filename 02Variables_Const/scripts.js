232e28d7b828a7ec7c0ff47aca8ffa4ba17a23a5
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-07 12:13:28 +0000

    4 topics learnt

---
+++ 02Variables_Const/scripts.js
@@ -0,0 +1,19 @@
+const accountId=144553; 
+// const ki value change nhi hoti
+let accountEmail="daminideshmukh2106@gmail.com"
+//memory m ek space reserve ho gyi h,let bhaut jyada use hota h
+var accountPassword="12345"
+//scope {} k issue k liye var k use km krte h
+accountCity="Jabalpur"
+//kr skte h pr mt krnaa
+let accountStaes;
+//iske karan undefined ayega print krne pr
+// accountId=3 //not allowed
+accountEmail="d1d@gmail.com"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStates])
+
+/*
+prefer not to use var because of issue in block scope and function scope
+*/
