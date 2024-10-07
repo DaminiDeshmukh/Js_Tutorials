83a0cf3d05b2553eac64e123976e13020c6d67fb
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-07 17:17:03 +0000

    more

---
+++ 03BASICS/datatypes1.js
@@ -0,0 +1,42 @@
+// let score=33
+// //number
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let score1="33a"
+// //string
+// console.log(typeof(score1));
+// console.log(typeof score1);
+
+// let valueInNumber=Number(score1)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//NaN refers to not a number
+// let a=true;
+// console.log(a);
+// console.log(typeof a)
+//"33"=>33
+//"33abc"=>Nan
+//true=>1;false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let a1=""
+let booleanA=Boolean(a1)
+console.log(booleanA)
+
+let a2="0"
+let booleanA2=Boolean(a2)
+console.log(booleanA2)
+
+//1=>true 0=>false
+//"" =>false
+//"hitesh"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
