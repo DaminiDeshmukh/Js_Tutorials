af71cb667f8a44b36663fc9b98322880c2867b5b
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-07 12:59:09 +0000

    3rd video

---
+++ 03BASICS/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser
+//js k engine browser m chupa h
+console.log(3+3); console.log("damini") //readability is low
+
+//mdn and tc39 are important
+let name="damini" //string
+let age=19
+let isLoggedIn=true
+
+//primitive data type
+
+//number =>2 to the power 53
+//bigint
+//string =>""
+//boolean =>true or false
+//null =>standalone value - kali value not khali string,temp 0 nhi rkh skte kyuki it means something so use null
+//undefined =>variable define kia h pr value nhi dia
+//symbol=>unique
+
+//object
+console.log(typeof"damini")
+console.log(typeof undefined)
+console.log(typeof null)
+
