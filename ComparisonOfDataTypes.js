1463b348a8a6d029fe20fd89dec6e69e737f4df1
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-08 06:12:39 +0000

    lesson 8

---
+++ ComparisonOfDataTypes.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+console.log("2">1);
+console.log("02">1);
+//lyu different datatypes ko use kre
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null|0);
+//comparison and equal operator is different
+//null is treated as a number 
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined|0);
+
+//these are avoided 
+
+console.log("2"===2);
