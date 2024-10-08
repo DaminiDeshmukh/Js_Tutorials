6fd0dfd8a0215441d0336d890738396ff02f3def
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-08 06:44:09 +0000

    LECTURE 10 AND 11

---
+++ l9DataTypesSummary.js
@@ -0,0 +1,19 @@
+// data types - 2 types -primitive and non-primitive =call by value and call by reference - based on how data is stored in sysytem
+//primitve - 7 
+/*
+string-jb inko copy krte h to inki location k refernce nhi dia jata,inko copy krkr dete h
+NUMBER
+BOOLEAN
+NULL- TEMP WALA YAAD KRNA
+UNDEFINED
+SYMBOL
+BigInt
+
+JS IS DYNAMIC TYPED LANGUAGE BECAUSE TYPE PHELE SE NHI BTATE.the type of a variable is determined at runtime, and you don't have to explicitly declare the type of a variable when you define it. Variables can hold values of any type, and the type can change as the program runs.
+
+reference(non-primitive)
+inka reference directly allocate kia jata h
+ARRAY
+OBJECT*(EVENTS BHI MASTER KR LOO*)
+FUNCTIONS
+*/
