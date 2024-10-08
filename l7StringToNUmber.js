ce80605f604dffc4a8a9058f2e049f5d853affb8
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-08 05:51:25 +0000

    lesson 7

---
+++ l7StringToNUmber.js
@@ -0,0 +1,27 @@
+// let value=3;
+// let negValue=-value;
+// console.log(negValue); 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+
+// let str1="hello";
+// let str2="world";
+// console.log(str1+str2); //hello world
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log(1+2+"3");
+// console.log((3+4)*5%3);
+console.log(true);
+console.log(+true);
+// console.log(true+);
+console.log(+"");
+let num,num2,num3
+num1=num2=num3=3+3
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);   
+//mdn k docs reder kro
