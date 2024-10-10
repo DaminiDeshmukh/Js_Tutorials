5c6b540f1d7f0fff7bee1a1ad4e72bfb917f1795
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-10 08:29:00 +0000

    stack and heap

--- l10StackAndHeapMemory.js
+++ l10StackAndHeapMemory.js
@@ -1,1 +1,25 @@
-#
+/*
+js m memory kese kaam krti h
+//stack memory
+-- used for primitive data type
+--copy milta h 
+//heap memory
+-- used for non- primitive data type
+--reference milta h 
+*/
+let myName="damini"
+let anotherName=myName
+anotherName="mini"
+console.log(myName);
+console.log(anotherName);
+
+let user=
+{
+    email: "xyz@gmail.com",
+    upi:"xyz@okhdfcbank"
+}
+let user1=user
+user1.email="damini@google.com"
+console.log(user.email);
+console.log(user1.email);
+
