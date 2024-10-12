cf06be0c1e80d8cf388c9c78323180380d2d91de
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-12 08:01:25 +0000

    one

---
+++ l26ControlFlow.js
@@ -0,0 +1,62 @@
+// // if
+// const isUseLoggedIn=true
+// const temperature=23
+// //== is used to compare the value . === is used to compare the value and type
+// if(temperature===20)
+// {
+//     console.log("temperature is 20 or less than 20")
+// }
+// else
+// {
+//     console.log("temperature is greater than 20")
+// }
+
+// console.log("execute")
+// // <,>,<=,>=,==,!=,===,!==
+
+// const score=200
+// if(score>100)
+// {
+//     let power="fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// //console.log(`user power:${power}`); // power is not defined
+
+
+// const balance=1000
+// if(balance>500) console.log("test"),console.log("test2");// if we have only one statement then we can write it without curly braces
+
+// if(balance<500)
+// {
+//     console.log("less than 500")
+// }
+// else if(balance<750)
+// {
+//     console.log("less than 750")
+// }
+// else if(balance <900)
+// {
+//     console.log("less than 750")
+// }
+// else
+// {
+//     console.log("less than 1200")
+// }
+
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmial=true
+
+if(userLoggedIn && debitCard && 2==3)
+{
+    console.log("allow to buy course");
+}
+
+if(loggedInFromGoogle||loggedInFromEmial)
+{
+    console.log("user logged in");
+}
