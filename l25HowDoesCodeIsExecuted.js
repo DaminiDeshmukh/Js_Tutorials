08c3cb623a9dd61b5910af1b804d2f6200504bae
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-12 07:47:55 +0000

    see again

---
+++ l25HowDoesCodeIsExecuted.js
@@ -0,0 +1,23 @@
+//code kese execute hoga in js ,call stack kese work karta hai
+//javascript execution context-jo bhi js file bnayi h woh run kese kregi
+//run krne k liye js 2 phase m run krti h
+
+//1.this k andr global execution context ko rkh dia jata h
+//node aur browser k andr global execution context alag hota h
+//browser m execution context important hota h-isme this ki value hoti h window(window object)
+
+//..1global execution
+
+//1. global execution context
+//2. function execution context
+
+//3. eval execution context(mongoose m)
+
+//..2memory creation phase-vo memory allocate krti h
+
+//..3execution phase-vo code run krti h
+
+//..4garbage collection-vo memory ko free krti h
+
+// see again
+
