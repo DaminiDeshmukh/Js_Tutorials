11a1d24f4f0b93e9e4ce64a5b6eb43d798755979
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-12 08:53:06 +0000

    .

---
+++ l28whileAndDoWhile.js
@@ -0,0 +1,18 @@
+let index=0;
+while(index<=10)
+{
+    console.log(`value of index is ${index}`);
+    index=index+1;
+}
+let myarray=['flash',"batman","suparman"]
+let arr=0;
+while(arr<myarray.length)
+{
+    console.log(`value is ${myarray[arr]}`);
+    arr=arr+1;
+}
+let score=11;
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score<=10);
