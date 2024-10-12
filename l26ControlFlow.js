46192acc8002e87ece44b2a90d4b78cd73b35fef
Damini Deshmukh <96333053+DaminiDeshmukh@users.noreply.github.com>
2024-10-12 08:05:05 +0000

    two

--- l26ControlFlow.js
+++ l26ControlFlow.js
@@ -60,3 +60,45 @@ if(loggedInFromGoogle||loggedInFromEmial)
 {
     console.log("user logged in");
 }
+
+
+
+
+
+
+
+
+
+//2 switch
+// switch(key)
+// {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month="march"
+// switch(month)
+// {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("february");
+//             break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case matched");
+//         break;
+
+        
+// }
